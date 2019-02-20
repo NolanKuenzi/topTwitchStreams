66301a0b72d6a607f360c456419370d2d7021fbb
Nolan Kuenzi <mr.nolank@gmail.com>
2019-02-20 02:15:12 -0800

    replaced fetch w/ axios

--- src/containers/twitchApi.js
+++ src/containers/twitchApi.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import regeneratorRuntime, { async } from 'regenerator-runtime';
+import axios from 'axios';
 import ClientId from '../clientId';
 
 const TwtichData = () => {
@@ -9,24 +10,16 @@ const TwtichData = () => {
   const [streamList, setStreamList] = useState(null);
   async function Api() {
     try {
-      /* Get the top streams */
       const getStreams = 'https://api.twitch.tv/helix/streams?first=50';
-      const request = await fetch(getStreams, {
-        headers: { 'Client-ID': ClientId },
-        method: 'GET',
-      });
-      const streamData = await request.json();
       const getGames = 'https://api.twitch.tv/helix/games/top?first=100';
+      /* Get the top streams */
+      const streamData = await axios.get(getStreams, { headers: { 'Client-ID': ClientId } });
       /* Get game names and box art urls */
-      const request2 = await fetch(getGames, {
-        headers: { 'Client-ID': ClientId },
-        method: 'GET',
-      });
-      const gameData = await request2.json();
-      /* Add numbers, game names, and box art urls to the streamData.data array */
-      streamData.data.forEach((item, index) => {
+      const gameData = await axios.get(getGames, { headers: { 'Client-ID': ClientId } });
+      /* Add numbers, game names, and box art urls to the streamData.data.data array */
+      streamData.data.data.forEach((item, index) => {
         item.number = index + 1;
-        const filterGameData = gameData.data.filter(game => game.id === item.game_id);
+        const filterGameData = gameData.data.data.filter(game => game.id === item.game_id);
         if (filterGameData.length < 1) {
           item.imgUrl = '';
           item.gameName = 'Unknown';
@@ -35,8 +28,8 @@ const TwtichData = () => {
         item.imgUrl = filterGameData[0].box_art_url;
         item.gameName = filterGameData[0].name;
       });
-      setApiData(streamData.data);
-      setOriginalData(streamData.data);
+      setApiData(streamData.data.data);
+      setOriginalData(streamData.data.data);
     } catch (error) {
       setApiData(null);
       alert('Data failed to load, please try again');
@@ -68,17 +61,17 @@ const TwtichData = () => {
               onClick={() => window.open(`https://www.twitch.tv/${item.user_name}`)}
             >
               <div className="internalDiv">
-                {`${item.number}.`} {item.user_name} - Viewer Count:{' '}
-                {formatNumber(item.viewer_count)}
-                <br /> <br />
-                Currently Streaming: {item.gameName}
-                <br />
+                  {`${item.number}.`} {item.user_name} - Viewer Count:{' '}
+                  {formatNumber(item.viewer_count)}
+                  <br /> <br />
+                  Currently Streaming: {item.gameName}
+                  <br />
                 <img
                   src={item.imgUrl.replace('{width}x{height}', '125x125')}
                   alt="Box Art is Unavailable"
                 />
                 <br /> <br />
-                Screen Shot:
+                  Screen Shot:
                 <br />
                 <img
                   src={item.thumbnail_url.replace('{width}x{height}', '350x300')}
