c0626e034d5edde08501cd8759d1908e314f13f0
Nolan Kuenzi <mr.nolank@gmail.com>
2019-02-23 19:40:49 -0800

    Updated Tests

--- src/containers/twitchApi.spec.js
+++ src/containers/twitchApi.spec.js
@@ -1,7 +1,6 @@
-/* eslint-disable */
 import React from 'react';
-import { render, cleanup, wait } from 'react-testing-library';
-import regeneratorRuntime, { async } from 'regenerator-runtime';
+import { render, cleanup, fireEvent, wait } from 'react-testing-library';
+import regeneratorRuntime, { async } from 'regenerator-runtime'; /* eslint-disable-line */
 import axios from 'axios';
 import TwitchData from './twitchApi';
 
@@ -10,6 +9,9 @@ jest.mock('axios');
 afterEach(cleanup);
 
 describe('<TwitchData /> Component', () => {
+  console.error = jest.fn(); /* eslint-disable-line */
+  /* Using this until there is a simpler work-around for testing async/await
+   without triggering the new `act` warning */
   test('The Api is called', () => {
     const axiosSpy = jest.spyOn(axios, 'get');
     render(<TwitchData />);
@@ -25,6 +27,24 @@ describe('<TwitchData /> Component', () => {
       expect(streamList.textContent).toContain('Apex Legends');
       expect(streamList.textContent).toContain('37,058');
       expect(streamList.textContent).toContain('34,662');
+    });
+  });
+  test('Typing in the input box filteres the data', async () => {
+    const { getByTestId } = render(<TwitchData />);
+    await wait(() => {
+      const input = getByTestId('testInput');
+      const streamList = getByTestId('streamListTest');
+      fireEvent.change(input, { target: { value: '' } });
+      expect(streamList.textContent).toContain('BeyondTheSummit');
+      expect(streamList.textContent).toContain('FemSteph');
+
+      fireEvent.change(input, { target: { value: 'OverwatchLeague' } });
+      expect(streamList.textContent).not.toContain('BeyondTheSummit');
+      expect(streamList.textContent).not.toContain('FemSteph');
+
+      fireEvent.change(input, { target: { value: 'FemSteph' } });
+      expect(streamList.textContent).toContain('FemSteph');
+      expect(streamList.textContent).not.toContain('BeyondTheSummit');
     });
   });
 });
